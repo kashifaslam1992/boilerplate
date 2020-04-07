3c125d295e1bdbb4cc7b93080a9a66dcb938c5d4
Hafiz Muhammad Kashif Aslam <kashif.aslam@systemsltd.com>
2020-04-07 18:14:03 +0500

    Boilerplate with gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,243 +1,174 @@
-/************************************/
-/* 			  Variables				*/
-/************************************/
-
-/*
- * Gulp Libraries
- * ...
- */	
-	var gulp = require('gulp'),
-	del = require('del'),
-	browsersync = require('browser-sync'),
-
-	imagemin = require('gulp-imagemin'),
-	newer = require('gulp-newer'),
-
-	njk = require('gulp-nunjucks'),
-	w3cjs = require('gulp-w3cjs'),
-
-	sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-
-
-    jshint = require('gulp-jshint'),
-	concat = require('gulp-concat'),
-	stripdebug = require('gulp-strip-debug'),
-	uglify = require('gulp-uglify'),
-
-
-
-/*
- * Soruce and Destination Folders
- * ...
- */
-    source = 'source/',
-	destination = 'build/',
-
-
-/*
- * Options List
- * ...
- */
-
- 	sassOptions = {
- 		errToLogConsole: true,
- 		precision: 4,
- 		outputStyle: 'expanded',
- 		sourceComments: false,
- 		indentWidth: 4
- 	},
-
-    autoprefixerOptions = {
- 		autoprefixer: {
- 			browsers: ['> 2%','last 3 versions'],
- 			cascade: false
- 		}
- 	},
-
- 	browsersyncOptions = {
-		server: {
-			baseDir: destination,
-			index: 'index.html'
-		},
-		open: true,
-		notify: true
-	},
-
-/*
- * Source and Destination Assets
- * ...
- */
-	html = {
-		in: source + '*.html',
-		out: destination
-	},
-
-	styles = {
-		in: source + 'sass/*.scss',
-		out: destination + 'css/'
-	},
-
-	stylesource = {
-		in: source + 'sass/**/*',
-		out: destination + 'sass/'
-	},
-
-	scripts = {
-		in: [
-			// Add All vendor paths here
-			source + 'js/*.js',
-			source + 'js/**/*.js'
-		],
-		out: destination + 'js/',
-		filename: 'main.js'
-	},
-
-	images = {
-		in: [source + 'images/*.*', source + 'images/**/*.*'],
-		out: destination + 'images/'
-	},
-
-	fonts = {
-		in: source + 'fonts/**/*',
-		out: destination + 'fonts/' 
-	},
-
-	watch = {
-		html: [source + '*.html', source + 'template/**/*.html'],
-		sass: [source + 'sass/**/*.scss'],
-		fonts: [source + 'fonts/*'],
-		images: [source + 'images/*.*', source + 'images/**/*.*'],
-		scripts: [source + 'js/*.js', source + 'js/**/*.js']
-	};	
-
 
 /************************************/
 /* 				Tasks				*/
 /************************************/
-
-
 /*
- * Task to clean the build folder
+ * Gulp Libraries
  * ...
  */
-gulp.task('cleanBuild', function(){
-	del([
-		destination + '*'
-	]);
-});
+
+var gulp = require('gulp'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    del = require('del'),
+    browserSync = require("browser-sync").create(),
+    newer = require('gulp-newer'),
+
+    sass = require('gulp-sass'),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+
+    plumber = require('gulp-plumber'),
 
 
-/*
- * Task for Browser Sync
- * ...
- */
-gulp.task('browsersync', function(){
-	browsersync(browsersyncOptions);
-});
+    /*
+     * Soruce and Destination Folders
+     * ...
+     */
+    source = 'src/',
+    destination = 'dist/',
 
-/*
- * Task to Build HTML from templates
- * ...
- */
-gulp.task('html', function(){
-	return gulp
-	.src(html.in)
-	.pipe(njk.compile())
+
+    /*
+     * Options List
+     * ...
+     */
+
+    sassOptions = {
+        errToLogConsole: true,
+        precision: 4,
+        outputStyle: 'expanded',
+        sourceComments: false,
+        indentWidth: 4
+    },
+
+    autoprefixerOptions = {
+        overrideBrowserslist: ['> 2%','last 3 versions'],
+        cascade: false
+    },
+
+    browsersyncOptions = {
+        server: {
+            baseDir: destination,
+            index: 'index.html'
+        },
+        open: true,
+        notify: true
+    },
+
+    /*
+     * Source and Destination Assets
+     * ...
+     */
+    html = {
+        in: source + 'html/*.html',
+        out: destination
+    },
+
+    css = {
+        in: source + 'sass/*.scss',
+        out: destination + 'css/'
+    },
+
+    cssSource = {
+        in: source + 'sass/**/*',
+        out: destination + 'sass/'
+    },
+
+    scripts = {
+        in: [
+            // Add All vendor paths here
+            source + 'js/*.js'
+        ],
+        out: destination + 'js/'
+    },
+
+    images = {
+        in: [source + 'images/*.*', source + 'images/**/*.*'],
+        out: destination + 'images/'
+    },
+
+    fonts = {
+        in: source + 'fonts/**/*',
+        out: destination + 'fonts/'
+    },
+
+    watcher = {
+        html: [source + '*.html', source + 'html/*.html', source + 'html/**/*.html'],
+        sass: [source + 'sass/**/*.scss'],
+        fonts: [source + 'fonts/*'],
+        images: [source + 'images/*.*', source + 'images/**/*.*'],
+        scripts: [source + 'js/*.js', source + 'js/**/*.js']
+    };
+
+
+function cleanBuild() {
+    return del([
+        destination + '*'
+    ]);
+}
+
+
+function nunjucks() {
+	return gulp.src(html.in)
+    .pipe(nunjucksRender({
+        path: [source+'/html/'] // String or Array
+    }))
 	.pipe(gulp.dest(html.out));
-});
+}
 
-/*
- * Task to Build HTML from templates and validate it
- * ...
- */
-gulp.task('htmlValidate', function(){
-	return gulp
-	.src(html.in)
-	.pipe(njk.compile())
-	.pipe(gulp.dest(html.out))
-	.pipe(w3cjs(html.out))
-	.pipe(w3cjs.reporter());
-});
+function style() {
+    return gulp.src(css.in)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions)).on('error', sass.logError)
+        .pipe(postcss([autoprefixer(autoprefixerOptions)]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(css.out))
+        .pipe(browserSync.stream());
+}
 
-/*
- * Task to Merge and Compile Sass files
- * ...
- */
-gulp.task('sass', function(){
-	return gulp.src(styles.in)
-	.pipe(sourcemaps.init())
-	.pipe(sass(sassOptions).on('error', sass.logError))
-	.pipe(sourcemaps.write())
-	.pipe(autoprefixer(autoprefixerOptions))
-	.pipe(gulp.dest(styles.out))
-	.pipe(browsersync.reload({stream: true}));
-});
+function graphics() {
+    return gulp.src(images.in)
+        .pipe(newer(images.out))
+        .pipe(gulp.dest(images.out));
+}
 
+function typography() {
+    return gulp.src(fonts.in)
+        .pipe(newer(fonts.out))
+        .pipe(gulp.dest(fonts.out));
+}
 
-/*
- * Task to compress and copy images into build folder
- * ...
- */
-gulp.task('images', function(){
-	return gulp
-	.src(images.in)
-	.pipe(newer(images.out))
-	.pipe(imagemin())
-	.pipe(gulp.dest(images.out));
-});
+function sassCopy() {
+    return gulp.src(cssSource.in)
+        .pipe(gulp.dest(cssSource.out));
+}
 
+function js() {
+    return gulp.src(scripts.in)
+        .pipe(plumber())
+        .pipe(gulp.dest(scripts.out));
+}
 
-/*
- * Task to copy Fonts in build folders
- * ...
- */
-gulp.task('fonts', function(){
-	return gulp
-	.src(fonts.in)
-	.pipe(newer(fonts.out))
-	.pipe(gulp.dest(fonts.out));
-});
+function watch() {
+    browserSync.init(browsersyncOptions);
+    gulp.watch(watcher.sass, gulp.series([style, sassCopy]));
+    gulp.watch(watcher.html, nunjucks);
+    gulp.watch(watcher.scripts, js);
+    gulp.watch(watcher.images, graphics);
+    gulp.watch(watcher.fonts, typography);
+    gulp.watch([html.out + '*.html', css.out + '*.css', scripts.out + '*.js', images.out + '*', fonts.out + '*']).on('change', browserSync.reload);
+}
 
-/*
- * Task to copy Fonts in build folders
- * ...
- */
-gulp.task('sassCopy', function(){
-	return gulp
-	.src(stylesource.in)
-	.pipe(gulp.dest(stylesource.out));
-});
+exports.cleanBuild = cleanBuild;
+exports.nunjucks = nunjucks;
+exports.style = style;
+exports.graphics = graphics;
+exports.typography = typography;
+exports.sassCopy = sassCopy;
+exports.js = js;
 
 
-/*
- * Task to copy Fonts in build folders
- * ...
- */
-gulp.task('scripts', function(){
-	return gulp
-	.src(scripts.in)
-	.pipe(newer(scripts.out))
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
-	// .pipe(jshint.reporter('fail'))
-	// .pipe(concat(scripts.filename))
-	// .pipe(stripdebug())
-	// .pipe(uglify())
-	.pipe(gulp.dest(scripts.out));
-});
+var build = gulp.parallel(watch);
 
-
-/*
- * Default Task
- * Watching All of the written tasks
- * ...
- */
-gulp.task('default', ['html', 'browsersync', 'sass', 'fonts', 'images', 'scripts', 'sassCopy'] , function() {
-	gulp.watch(watch.html, ['html', browsersync.reload]);
-	gulp.watch(watch.sass,['sass', 'sassCopy']);
-	gulp.watch(watch.fonts, ['fonts']);
-	gulp.watch(watch.images, ['images']);
-	gulp.watch(watch.scripts, ['scripts', browsersync.reload]);
-});
+gulp.task('default', build);
--- readme.md
+++ readme.md
@@ -1,61 +1,28 @@
 # Installations
 
 - install Node.js [Node.js](https://nodejs.org/en/)
--  Extract ruby and its dev kit [Ruby](https://rubyinstaller.org/).
-    - You can find the [complete guide for windows here](https://forwardhq.com/help/installing-ruby-windows)
-- Install ruby gem compass [Compass](https://rubygems.org/search?utf8=%E2%9C%93&query=compass)
-    - **Command => gem install compass**
+- install gulp and gulp-cli Globally **npm i -g gulp gulp-cli**
 - Now install dependencies and package wit this command
     - **Command => npm install**
-- If you want to use [Bower](https://bower.io/) as package manager, Run these two commands in the project folder to setup bower.
-    - **Command=> npm install -g bower**
-    - **Command=> bower init and save with default**
-- Now to install any bower package run this commmand
-    - **Command=> bower install <packagename> -S**
 
 
 # Directory Structure
 
-- All of your working files will be in source folder and it will output the file in build folder
-- Add all the font file in source->fonts
-- Add all the image files in source->images and the image which you want to convert to data-uri to avoid http calls, add them into source->images->inline
-- Add all the js file in source->js for libraries use lib folder
-- Add all the .scss file in source->sass
-- Add all the html partials file in source->template
-
-
-# Gulp Packages
-
-- **[gulp](https://www.npmjs.com/package/gulp):** The streaming build system
-- **[bower](https://www.npmjs.com/package/bower):** The browser package manager
-- **[del](https://www.npmjs.com/package/del):** Delete files and folders
-- **[browser-sync](https://www.npmjs.com/package/browser-sync):** Live CSS Reload & Browser Syncing
-- **[gulp-nunjucks](https://www.npmjs.com/package/gulp-nunjucks):** HTML templating engine
-- **[gulp-sass](https://www.npmjs.com/package/gulp-sass):** Gulp plugin for sass
-- **[gulp-pleeease](https://www.npmjs.com/package/gulp-pleeease):** To add vendor prefixes automatically
-- **[gulp-imagemin](https://www.npmjs.com/package/gulp-imagemin):** Minify PNG, JPEG, GIF and SVG images
-- **[gulp-newer](https://www.npmjs.com/package/gulp-newer):** Only pass through newer source files
-- **[gulp-imacss](https://www.npmjs.com/package/gulp-imacss):** A gulp plugin for using imacss (the image to datauri to CSS transformer) to avoid http calls
-- **[gulp-jshint](https://www.npmjs.com/package/gulp-jshint):** JSHint plugin for gulp To log js error in console
-- **[gulp-concat](https://www.npmjs.com/package/gulp-concat):** Concatenates files
-- **[gulp-strip-debug](https://www.npmjs.com/package/gulp-strip-debug):** Strip console and debugger statements from JavaScript code
-- **[gulp-uglify](https://www.npmjs.com/package/gulp-uglify):** Minify files with UglifyJS.
-
-
-# Load components from bower
-
-- To load any js file from bower_components add the path of js file in array scripits.in array in gulpfile.js file (find this: //add path of any bower component here)
-- To load and sass file in css file add the path of sass file at in you main scss file in sass folder
+- All of your working files will be in src folder and it will output the file in dist folder
+- Add all the font file in src->fonts
+- Add all the image files in src->images
+- Add all the js file in src->js for libraries use lib folder
+- Add all the .scss file in src->sass
+- Add all the html partials file in src->html
 
 
 # Gulp Tasks
 
-- **watch:** Runs all the tasks automatically and waits for any change
+- **gulp:** Runs all the tasks automatically and waits for any change
 - **cleanBuild:** Cleans the build folder
 - **browsersync:** For reload and mobile testing
 - **html:** To merge the html partials and output the complete html files
 - **sass:** Complies sass to css
 - **images:** Minimizes, copies new images into build folder
-- **imageuri:** Change inline images to data uri
 - **fonts:** Copies new font files to build folder
 - **scripts:** Copies and debugs the script files
